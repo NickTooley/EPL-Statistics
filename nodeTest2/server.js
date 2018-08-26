822807a6096f22ea5ac7f9154bdd19941751602a
Nick Tooley <toolnj1@student.op.ac.nz>
2018-08-27 03:55:38 +1200

    Added Node.JS

---
+++ nodeTest2/server.js
@@ -0,0 +1,60 @@
+var express = require ('express');
+var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: false }));
+
+mongoose.Promise = Promise
+
+var dbUrl = 'mongodb://ntooley:user123@ds147391.mlab.com:47391/node-test-tooley'
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
+
+app.get('/messages', (req, res)=>{
+    Message.find({}, (err, messages)=>{
+        res.send(messages);
+    })
+})
+
+app.post('/messages', (req, res)=>{
+    var message = new Message(req.body)
+
+    message.save().then(() => {
+
+        Message.findOne({message: 'badword'}, (err, censored)=>{
+            if(censored){
+            console.log('censored word find', censored)
+            Message.remove({_id: censored.id}, (err) => {
+                    console.log("removed bad word")
+            })
+        }
+        })
+         io.emit('message', req.body)
+        res.sendStatus(200)
+    }).catch((err) => {
+        res.sendStatus(500)
+        return console.error(err)
+    })
+    
+})
+
+io.on('connection', (socket) => {
+    console.log('A user is connected')
+})
+
+mongoose.connect(dbUrl,{ useNewUrlParser: true }, (err) => {
+    console.log('db connection', err)
+})
+
+var server = http.listen(3000, () => {
+    console.log('server is listening on port ', server.address().port)
+});
+
