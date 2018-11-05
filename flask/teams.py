a31a74c04ceec6853697b281b7cfc78b8e9777ac
Nick Tooley <toolnj1@student.op.ac.nz>
2018-11-06 02:39:55 +1300

    Added team graphs as well as removal and readding

--- flask/teams.py
+++ flask/teams.py
@@ -6,9 +6,10 @@ bp = Blueprint('teams', __name__)
 
 @bp.route('/teams/<string:team>')
 def show_team(team):
-    teamdata = Data.objects(teamID=team)
+    teamdata = Data.objects(teamID=team).first()
+    allData = Data.objects.all() 
 
-    return render_template('base.html')
+    return render_template('teams/teams.html', dataset=allData, teamData=teamdata)
 
 @bp.route('/')
 def show_league():
